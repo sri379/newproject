6a9cfaf7c1e010b5941be28d733a0a019079a8c4
sri697 <sri694040@gmail.com>
2023-11-17 15:29:12 +0530

    login css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-// Login.js
+
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import users from '../../data/User';
-import './Login.css'; 
+import './Login.css';
 import { ToastContainer, Zoom, toast } from 'react-toastify';
 
 const Login = ({ isAuth, setIsAuth }) => {
@@ -15,13 +16,11 @@ const Login = ({ isAuth, setIsAuth }) => {
     const user = users.find((user) => user.name === name && user.password === password);
     if (user) {
       user.isLogin = true;
-
       localStorage.setItem('users', JSON.stringify(users));
       localStorage.setItem('currentUser', JSON.stringify(user));
       setIsAuth(user);
 
       if (user.name === 'Admin') {
-        // If the user is an admin, navigate to the admin dashboard or any desired route
         navigate('/admin-dashboard');
       } else {
         navigate('/employee-list');
@@ -33,41 +32,43 @@ const Login = ({ isAuth, setIsAuth }) => {
 
   return (
     <div className="login-body">
-      <ToastContainer position='bottom-right' transition={Zoom} autoClose={4000} closeOnClick={false} />
+      <ToastContainer position="bottom-right" transition={Zoom} autoClose={4000} closeOnClick={false} />
       <div className="login-container">
-        <h2 className="log-heading">Welcome to TimeForge Portal!</h2>
-        <h1 className="login-heading">Login</h1>
-        <label className="login-label">
-          Name:
-          <input
-            className="login-text"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <br />
-        <label className="login-label">
-          Password:
-          <div className="password-input">
+        <div className="login-left"></div>
+        <div className="login-right">
+          <h2 className="log-heading">TimeForge Portal!</h2>
+          <h2 className="log-heading1">Welcome Back</h2>
+          <h1 className="login-heading">Login</h1>
+          <div className="input-group">
+            <label className="login-label">Name:</label>
+            <input
+              className="login-text"
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="input-group password-input">
+            <label className="login-label">Password:</label>
             <input
               className="login-password"
               type={showPassword ? 'text' : 'password'}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            Show Password
-            <input
-              type="checkbox"
-              checked={showPassword}
-              onChange={() => setShowPassword(!showPassword)}
-            />
+            <div className>
+              <input    
+                type="checkbox"
+                checked={showPassword}
+                onChange={() => setShowPassword(!showPassword)}
+              />
+              
+            </div>
           </div>
-        </label>
-        <br />
-        <button className="button-login" onClick={handleLogin}>
-          Login
-        </button>
+          <button className="button-login" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
