cf0287f4fb5a6af75d05517782770b8705dee9ae
sri697 <sri694040@gmail.com>
2023-11-20 17:59:16 +0530

    remember me

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import users from '../../data/User';
 import './Login.css';
 import { ToastContainer, Zoom, toast } from 'react-toastify';
+import { Button, Form, OverlayTrigger, Tooltip, Alert } from 'react-bootstrap';
 
 const Login = ({ isAuth, setIsAuth }) => {
   const navigate = useNavigate();
+  const [rememberMe, setRememberMe] = useState(false);
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    const rememberMePreference = localStorage.getItem('rememberMe');
+    setRememberMe(rememberMePreference === 'true');
+  }, []);
 
   const handleLogin = () => {
     const user = users.find((user) => user.name === name && user.password === password);
@@ -26,10 +33,7 @@ const Login = ({ isAuth, setIsAuth }) => {
       toast.error('Invalid Login Credentials');
     }
   };
-  const generateSpaces = (count) => {
-    return Array(count).fill('&nbsp;').join('');
-  };
-  const indentation = generateSpaces(12);  
+
   return (
     <div className="login-body">
       <ToastContainer position="bottom-right" transition={Zoom} autoClose={4000} closeOnClick={false} />
@@ -37,10 +41,9 @@ const Login = ({ isAuth, setIsAuth }) => {
         <div className="login-left"></div>
         <div className="login-right">
           <h2 className="log-heading">Time Forge Portal!</h2>
-
           <h1 className="login-heading">Login</h1>
           <div className="input-group">
-              <label className="login-label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Name:</label>
+            <label className="login-label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Name:</label>
             <input
               className="login-text"
               type="text"
@@ -49,12 +52,21 @@ const Login = ({ isAuth, setIsAuth }) => {
             />
           </div>
           <div className="input-group password-input">
-            <label className="login-label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password:</label>
+            <label className="login-label">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Password:</label>
             <input
               className="login-password"
               type="password" // Always show dots for the password
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="remember-me-checkbox">
+            <Form.Check
+              type="checkbox"
+              id="form.ControlInput3"
+              label="Remember me"
+              checked={rememberMe}
+              onChange={() => setRememberMe(!rememberMe)}
             />
           </div>
           <button className="button-login" onClick={handleLogin}>
